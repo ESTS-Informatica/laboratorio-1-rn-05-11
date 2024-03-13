0b57fc4fdf940346bf2790844b7a48eb08880751
MiguelSilva202200034 <202200034@estudantes.ips.pt>
2024-03-13 10:18:40 +0000

    Nivel 3

---
+++ WordGenerator.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Write a description of class WordGenerator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WordGenerator
+{
+    private ArrayList<String> words;
+    public WordGenerator()
+    {
+        words = new ArrayList<>();
+    }
+    
+    private void fillArrayList(){ 
+        words.add("boolean");
+        words.add("break");
+        words.add("byte");
+        words.add("case");
+        words.add("char");
+        words.add("class");
+        words.add("continue");
+        words.add("do");
+        words.add("double");
+        words.add("else");
+        words.add("enum");
+        words.add("for");
+        words.add("if");
+        words.add("import");
+        words.add("int");
+    }
+    
+    public String generateWord(){
+        Random random = new Random();
+        int number = random.nextInt(words.size());
+        return words.get(number);
+    }
+    
+    public void addWord(String word){
+        words.add(word);
+    }
+}
--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -10,9 +10,11 @@ public class WordGuessingGame
     private String hiddenWord;
     private String guessedWord;
     private int numberOfTries;
+    private InputReader reader;
     
     public WordGuessingGame()
     {
+        reader = new InputReader();
         hiddenWord = "abc";
         guessedWord = "___";
     }
@@ -29,7 +31,43 @@ public class WordGuessingGame
         return numberOfTries;
     }
     
-    public void showGuessedWord(){
+    private void showGuessedWord(){
         System.out.println("Palavra adivinhada: " + guessedWord);
     }
+    
+    private void showWelcome(){
+        System.out.println("Bem-vindo, tente adivinhar a palavra!");
+    }
+    
+    private boolean guess (char guessedLetter){
+        if(guessedWord.indexOf(guessedLetter) >= 0){
+           System.out.println("Essa letra já foi adivinhada!");
+           numberOfTries++;
+           return false;
+        } else if(hiddenWord.indexOf(guessedLetter) >= 0){
+           int indexOfGuessedLetter = hiddenWord.indexOf(guessedLetter); 
+           guessedWord = guessedWord.substring(0, indexOfGuessedLetter) +
+           guessedLetter + guessedWord.substring(indexOfGuessedLetter + 1);
+           numberOfTries++;
+           return true;
+ 
+        }
+        System.out.println("Essa letra não pertence à palavra"); 
+        return false;
+    }
+    
+    private void showResult(){
+        System.out.println("Jogo concluido!");
+        System.out.println("Número de tentativas = " + numberOfTries);
+    }
+    
+    public void play(){
+        do {
+          char guessedChar = reader.getChar("Introduza uma letra: ");
+        guess(guessedChar);
+          
+        } while (!guessedWord.equals(hiddenWord));
+        showResult();
+    }
+    
 }
